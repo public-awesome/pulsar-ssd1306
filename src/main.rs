ca855ae77fc6d8efed21c550df638dd92128f033
Shane Vitarana <shanev@gmail.com>
2020-11-01 12:29:03 -0500

    Displaying Hello World!

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use embedded_graphics::{
     fonts::{Font6x8, Text},
     pixelcolor::BinaryColor,
     prelude::*,
-    style::{TextStyleBuilder},
+    style::TextStyleBuilder,
 };
 use linux_embedded_hal::I2cdev;
-use ssd1306::{mode::GraphicsMode, Builder, I2CDIBuilder};
+use ssd1306::{Builder, I2CDIBuilder, displaysize::DisplaySize128x32, mode::GraphicsMode};
 use std::thread::sleep;
 use std::time::Duration;
 extern crate ctrlc;
@@ -23,7 +23,10 @@ fn main() {
     let i2c = I2cdev::new("/dev/i2c-1").unwrap();
 
     let interface = I2CDIBuilder::new().init(i2c);
-    let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
+    let mut disp: GraphicsMode<_, _> = Builder::new()
+        .size(DisplaySize128x32)
+        .connect(interface)
+        .into();
 
     disp.init().unwrap();
     disp.flush().unwrap();
@@ -34,13 +37,10 @@ fn main() {
         .build();
 
     while running.load(Ordering::SeqCst) {
-        Text::new(
-            "Hello world!",
-            Point::new(0, 56),
-        )
-        .into_styled(text_style)
-        .into_iter()
-        .draw(&mut disp);
+        Text::new("Hello world!", Point::zero())
+            .into_styled(text_style)
+            .draw(&mut disp)
+            .unwrap();
 
         disp.flush().unwrap();
 
