811a1fbb2c0ecad60815f37f3da6ab9db2f3de46
Shane Vitarana <shanev@gmail.com>
2020-11-01 10:05:25 -0500

    Initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    style::{TextStyleBuilder},
+    DrawTarget,
+};
+use linux_embedded_hal::I2cdev;
+use ssd1306::{mode::GraphicsMode, Builder, I2CDIBuilder};
+use std::thread::sleep;
+use std::time::Duration;
+extern crate ctrlc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    let i2c = I2cdev::new("/dev/i2c-1").unwrap();
+
+    let interface = I2CDIBuilder::new().init(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    let text_style = TextStyleBuilder::new(Font6x8)
+        .text_color(BinaryColor::On)
+        .background_color(BinaryColor::Off)
+        .build();
+
+    while running.load(Ordering::SeqCst) {
+        Text::new(
+            "Hello world!",
+            Point::new(0, 56),
+        )
+        .into_styled(text_style)
+        .into_iter()
+        .draw(&mut disp);
+
+        disp.flush().unwrap();
+
+        sleep(Duration::from_secs(2));
+
+        disp.clear();
+    }
+    disp.clear();
+    disp.flush().unwrap();
 }
